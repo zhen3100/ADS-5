c195a3aa90eada1e81c8c43f69ee00880011a273
anton <ashtanyuk@gmail.com>
2022-03-20 17:45:58 +0300

    Начальный коммит

---
+++ include/tstack.h
@@ -0,0 +1,9 @@
+// Copyright 2021 NNTU-CS
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
+
+template<typename T, int size>
+class TStack {
+
+};
+#endif  // INCLUDE_TSTACK_H_
---
+++ src/alg.cpp
@@ -0,0 +1,14 @@
+// Copyright 2021 NNTU-CS
+#include <string>
+#include <map>
+#include "tstack.h"
+
+std::string infx2pstfx(std::string inf) {
+  // добавьте код
+  return std::string("");
+}
+
+int eval(std::string pref) {
+  // добавьте код
+  return 0;
+}
