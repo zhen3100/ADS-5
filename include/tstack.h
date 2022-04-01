9109303faef90ca2d8dd5d8fbfc8e27be4f3d2f2
Evgeny Volkov <88347990+zhen3100@users.noreply.github.com>
2022-04-02 00:08:06 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,9 +1,29 @@
-// Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
 template<typename T, int size>
 class TStack {
-
+ private:
+     T arr[100];
+     int top;
+ public:
+     TStack() :top(-1) { }
+    T get() const {
+        return arr[top];
+    }
+    bool isEmpty() const {
+        return top == -1;
+    }
+    bool isFull() const {
+        return top == size - 1;
+    }
+    void pop() {
+        if (top >= 0)
+            top--;
+    }
+    void push(T item) {
+        if (top < size - 1)
+            arr[++top] = item;
+    }
 };
 #endif  // INCLUDE_TSTACK_H_
